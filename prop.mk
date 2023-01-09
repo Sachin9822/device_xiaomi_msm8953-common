5bd33212ee7cbfaf5c2a605f5abfa233422da058
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2023-01-09 05:15:23 +0000

    msm8953-common: remove DEVICE_PROVISIONED

--- prop.mk
+++ prop.mk
@@ -180,7 +180,6 @@ persist.vendor.data.mode=concurrent
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
